c6cd8272b3bfb1e1a590b290ac167e71abe0e4e9
Nargiza Sova <nargizkin@yandex.ru>
2023-08-28 10:40:17 +0300

    add str_func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def title(our_word):
+    """функция выведет все слова заглавными буквами"""
     return our_word.upper()
+
+def first_letters_up(our_word):
+    """function takes input and returns the word with first capital letters"""
+    return our_word.title()
